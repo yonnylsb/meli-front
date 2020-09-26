51a0f4f3f2536a3b103469a260af311d05ca36dd
Yonny Sanchez <yonny.sanchez@globant.com>
2020-09-25 20:28:31 -0500

    Using webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./components/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const wrapper = document.getElementById("container");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
